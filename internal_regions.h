b581a2062c2ab8b110ea0827bf0ef031d1097af7
Watty182 <watty182@gmail.com>
2017-04-20 14:37:29 -0500

    Add more prototypes and forgotten includes

--- internal_regions.h
+++ internal_regions.h
@@ -1,10 +1,17 @@
 #ifndef _INTERNAL_REGIONS_H
 #define _INTERNAL_REGIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "regions.h"
+
 #define MOD8 8
 
 r_size_t roundToEight( r_size_t, int );
 region** regionSearch( const char* );
 block** blockSearch( region*, void* );
 Boolean freeBlock( block** );
+void validationStation();
 #endif
