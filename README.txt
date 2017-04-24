e71f648e92c2c832007b891ee9718be15345d3d2
Watty182 <watty182@gmail.com>
2017-04-24 11:15:04 -0500

    Create test framework and README

---
+++ README.txt
@@ -0,0 +1,10 @@
+README
+A4
+
+Run program with:
+
+make regions_test; ./regions_test
+
+VERSIONS
+
+0.9: first release success with tests
--- main.c
+++ main.c
@@ -6,59 +6,32 @@
 
 // this code should run to completion with the output shown
 // you must think of additional cases of correct use and misuse for your testing
+int testsPassed = 0;
+int testsFailed = 0;
+
 int main()
 {
     Boolean rc;
-    int *ia;
-    char *ca1, *ca2, *ca3, *ca4;
-    char *fail;
 
-    rc = rinit("hello", 1024);
-    assert(rc);
-    rc = rinit("world", 798); // 800
-    assert(rc);
-
-    printf("Chosen: %s\n", rchosen()); // world
-
-    rc = rchoose("hello");
-    assert(rc);
-    ia = ralloc(sizeof(int) * 32);
-    printf("Size: %d\n", rsize(ia)); // 128
-    ca1 = ralloc(256);
-    assert(NULL != ca1);
-    ca2 = ralloc(384);
-    assert(NULL != ca2);
-    fail = ralloc(384); // not enough memory
-    assert(NULL == fail);
-    rc = rfree(ca1);
-    assert(rc);
-    fail = ralloc(384); // not enough contiguous memory
-    assert(NULL == fail);
-    rc = rfree(ia);
-    assert(rc);
-    ca3 = ralloc(384); // now there's enough memory
-    assert(NULL != ca3);
-
-    rc = rchoose("world");
-    assert(rc);
-    ca4 = ralloc(796);
-    assert(NULL != ca4);
-    printf("Size: %d\n", rsize(ca4)); // 800
-
-    rdump(); // hello & world
-
-    rdestroy("hello");
-
-    rc = rfree(ca4 + 24); // not the start of the block
+    rc = rinit("", 300); //disallowed
     assert(!rc);
-    rc = rfree(ca4); // better!
+    rc = rinit("test2", 298);
     assert(rc);
+    rc = rinit("test2", 800); //fail; same name
+    assert(!rc);
+    rc = rinit("test3", 0); //fail; zero size
+    assert(!rc);
+    rc = rinit("test4", 1); //pass; size 8
+    assert(rc);
+    rc = rchoose("");
+    assert(!rc); 
+    rc = rchoose("test2"); //pass
+    assert(rc);
+    rc = rchoose("dne"); //does not exist
+    assert(!rc);
 
-    rdestroy("world");
+    printf("Program Completed Successfully");    
 
-    rdump(); // nothing
-
-    fprintf(stderr, "\nEnd of processing.\n");
-
-    return EXIT_SUCCESS;
 }
+
+
